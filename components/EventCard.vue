4eddf8bb046a197996ed3d16544f69cb8c51bd69
swrzalek <sebastian.wrzalek@limebrains.com>
2021-06-28 16:31:16 +0200

    Mainpage container

---
+++ components/EventCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="py-4 px-8 rounded-2xl md:my-10 my-5 card">
+    <div>
+      <h2 class="text-white text-3xl font-semibold">Design Tools</h2>
+      <p class="mt-2 text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
+        deserunt ea doloremque natus error, rerum quas odio quaerat nam ex
+        commodi hic, suscipit in a veritatis pariatur minus consequuntur!
+      </p>
+    </div>
+    <div class="flex justify-end mt-4">
+      <button
+        type="button"
+        class="
+          border border-gray-300
+          text-white
+          rounded-md
+          px-4
+          py-2
+          m-2
+          transition
+          duration-500
+          ease
+          select-none
+          hover:bg-gray-200
+          focus:outline-none focus:shadow-outline
+        "
+      >
+        Show more
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+}
+</script>
+
+<style scoped>
+.card {
+  background: rgba(64, 64, 64, 0.2);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(6px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,102 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">feest</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container-outer">
+    <div class="container-inner">
+      <div class="container mx-auto px-4">
+        <div>
+          <span class="text-white text-3xl font-semibold">Festivals</span>
+          <span class="text-white text-opacity-50 text-3xl font-semibold mx-4"
+            >Concerts</span
+          >
+          <span class="text-white text-opacity-50 text-3xl font-semibold"
+            >Cultural Events</span
+          >
+        </div>
+        <div>
+          <div
+            class="border border-gray-300 text-white rounded-md inline-block"
+          >
+            <div
+              class="
+                rounded-l-md
+                hover:bg-white hover:text-gray-300 hover:bg-opacity-25
+                border-r
+                cursor-pointer
+                inline-block
+                px-4
+                py-2
+                transition
+                duration-500
+                ease
+                select-none
+                focus:outline-none focus:shadow-outline
+              "
+            >
+              Location
+            </div>
+            <div
+              class="
+                hover:bg-white hover:text-gray-300 hover:bg-opacity-25
+                cursor-pointer
+                inline-block
+                px-4
+                py-2
+                transition
+                duration-500
+                ease
+                select-none
+                focus:outline-none focus:shadow-outline
+              "
+            >
+              Genre
+            </div>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 gap-4 justify-items-center">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import EventCard from '../components/EventCard'
+export default {
+  components: {
+    EventCard,
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  &-outer {
+    margin: 0 auto;
+    min-height: 100vh;
+    min-width: 100%;
+    background: url('../assets/bg.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  &-cover {
+    min-height: 100vh;
+    background: linear-gradient(
+      126.6deg,
+      rgba(255, 255, 255, 0.12) 28.69%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    backdrop-filter: blur(1px);
+  }
 }
 </style>
